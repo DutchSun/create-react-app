507c3273891b60dba567f47fd56a4845a861b507
sunqi <qisun@harmonycloud.cn>
2020-11-20 15:09:28 +0800

    feat: 增加antd主题定制配置

--- packages/cra-template/template/config-overrides.js
+++ packages/cra-template/template/config-overrides.js
@@ -1,10 +1,18 @@
 /* config-overrides.js */
 const path = require('path');
-const { override, addWebpackExternals, addWebpackAlias } = require('customize-cra');
+const { override, addWebpackExternals, addWebpackAlias, addLessLoader } = require('customize-cra');
 
 module.exports = override(
 	addWebpackExternals({
 		React: 'React'
+	}),
+	addLessLoader({
+		lessOptions: {
+			modifyVars: {
+				// 'primary-color': '#fff'
+			},
+			javascriptEnabled: true
+		}
 	}),
 	addWebpackAlias({
 		'@': path.resolve(__dirname, 'src')
