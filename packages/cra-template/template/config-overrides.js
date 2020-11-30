66e5e87b6d2b14f4bf91d19b3a4c64171fa7751d
sunqi <qisun@harmonycloud.cn>
2020-11-30 15:14:20 +0800

    chore: 增加CORS配置;ESLint配置

--- packages/cra-template/template/config-overrides.js
+++ packages/cra-template/template/config-overrides.js
@@ -1,20 +1,32 @@
 /* config-overrides.js */
 const path = require('path');
-const { override, addWebpackExternals, addWebpackAlias, addLessLoader } = require('customize-cra');
+const { override, addWebpackExternals, addWebpackAlias, addLessLoader, overrideDevServer } = require('customize-cra');
 
-module.exports = override(
-	addWebpackExternals({
-		React: 'React'
-	}),
-	addLessLoader({
-		lessOptions: {
-			modifyVars: {
-				// 'primary-color': '#fff'
-			},
-			javascriptEnabled: true
-		}
-	}),
-	addWebpackAlias({
-		'@': path.resolve(__dirname, 'src')
-	})
-);
+// 本地开发时允许主框架跨域访问
+const allowCORS = () => config => {
+	// Default config: https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/config/webpackDevServer.config.js
+	config.headers = { 'Access-Control-Allow-Origin': '*' };
+	return config
+}
+
+module.exports = {
+	webpack: override(
+		addWebpackExternals({
+			React: 'React'
+		}),
+		addLessLoader({
+			lessOptions: {
+				modifyVars: {
+					// 'primary-color': '#fff'
+				},
+				javascriptEnabled: true
+			}
+		}),
+		addWebpackAlias({
+			'@': path.resolve(__dirname, 'src')
+		})
+	),
+	devServer: overrideDevServer(
+		allowCORS()
+	)
+}
